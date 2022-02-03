dadb7b3da4f1b4fa662215f22dbf87f1e8e40055
bohongu <91203029+bohongu@users.noreply.github.com>
2022-02-03 21:22:42 +0900

    Update BOJ_2845.java

--- src/baekjoon/BOJ_2845.java
+++ src/baekjoon/BOJ_2845.java
@@ -1,9 +1,20 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class BOJ_2845 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int l = sc.nextInt();
+		int p = sc.nextInt();
+		int sum = l*p;
+		
+		
+		for(int i=0;i<5;i++) {
+			short j = sc.nextShort();
+			System.out.print(j-sum + " ");
+		}
 
 	}
 
