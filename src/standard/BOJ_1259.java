4fb8a433df943624f23b3bd6ddb08da0be9ca980
bohongu <91203029+bohongu@users.noreply.github.com>
2022-03-08 20:04:24 +0900

    BOJ Update

---
+++ src/standard/BOJ_1259.java
@@ -0,0 +1,32 @@
+package standard;
+
+import java.util.Scanner;
+
+public class BOJ_1259 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		while(true) {
+			String s = sc.next();
+			if(s.equals("0")) {
+				break;
+			} else {	
+				int cnt = 0;
+				for(int i=0;i<s.length()/2;i++) {
+					if(s.charAt(i)==s.charAt(s.length()-i-1)) {
+						cnt++;
+					}
+				}
+				
+				if(cnt == s.length()/2) {
+					System.out.println("yes");
+				} else {
+					System.out.println("no");
+				}
+			}	
+		}
+	}
+}
