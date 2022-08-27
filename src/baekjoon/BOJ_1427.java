0f4854205bfad9a346a6b0bc43f63f96ab67c61e
bohongu <jaehong2782@naver.com>
2022-08-27 22:15:58 +0900

    Create BOJ_1427.java

---
+++ src/baekjoon/BOJ_1427.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class BOJ_1427 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		Integer[] arr = new Integer[n.length()];
+		
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = (int) n.charAt(i)-48;
+		}
+		
+		Arrays.sort(arr,Collections.reverseOrder());
+		for(Integer s : arr) {
+			System.out.print(s);
+		}
+	}
+
+}
