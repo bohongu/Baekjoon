85f042a3005091fc80756821af2b8c6edcbe590c
bohongu <jaehong2782@naver.com>
2023-04-05 12:22:38 +0900

    feat: add algorithm template

--- Algorithm/index.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "/input.txt";
-const input = fs.readFileSync(__dirname + filePath).toString();
-
-console.log();
