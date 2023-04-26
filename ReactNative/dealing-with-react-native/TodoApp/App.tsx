2db0bf322304ad3bf5bdacfab3be7d9c40a7b80d
bohongu <jaehong2782@naver.com>
2023-04-26 18:11:05 +0900

    feat: TodoApp 1

--- ReactNative/dealing-with-react-native/TodoApp/App.tsx
+++ ReactNative/dealing-with-react-native/TodoApp/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {KeyboardAvoidingView, Platform, StyleSheet} from 'react-native';
+import DateHead from './components/DateHead';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
 
 const App = () => {
+  const today = new Date();
+
   return (
-    <View>
-      <Text>하이</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <KeyboardAvoidingView
+          behavior={Platform.select({ios: 'padding'})}
+          style={styles.avoid}>
+          <DateHead date={today} />
+          <Empty />
+          <AddTodo />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  avoid: {
+    flex: 1,
+  },
+});
 
 export default App;
