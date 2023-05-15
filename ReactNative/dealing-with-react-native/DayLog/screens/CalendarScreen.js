e9fe969933f6cb9e9e2a4af856251a0398a20687
bohongu <jaehong2782@naver.com>
2023-05-15 19:59:59 +0900

    feat: update DayLog2

--- ReactNative/dealing-with-react-native/DayLog/screens/CalendarScreen.js
+++ ReactNative/dealing-with-react-native/DayLog/screens/CalendarScreen.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {Animated, StyleSheet, View, Button} from 'react-native';
+
+function FadeInAndOut() {
+  const animation = useRef(new Animated.Value(1)).current;
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: enabled ? 1 : 0,
+      useNativeDriver: true,
+    }).start();
+  }, [animation, enabled]);
+
+  return (
+    <View>
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 150],
+                }),
+              },
+            ],
+            opacity: animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+        ]}
+      />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setEnabled(!enabled);
+        }}
+      />
+    </View>
+  );
+}
 
 function CalendarScreen() {
-  return <View style={styles.block} />;
+  return (
+    <View style={styles.block}>
+      <FadeInAndOut />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {},
+  rectangle: {width: 100, height: 100, backgroundColor: 'black'},
 });
 
 export default CalendarScreen;
