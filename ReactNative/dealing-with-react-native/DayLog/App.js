3296cf3755ad5539e53cff442d046ecaa7b57a24
bohongu <jaehong2782@naver.com>
2023-05-17 11:39:47 +0900

    feat: DayLog Search

--- ReactNative/dealing-with-react-native/DayLog/App.js
+++ ReactNative/dealing-with-react-native/DayLog/App.js
@@ -2,13 +2,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import RootStack from './screens/RootStack';
 import {LogContextProvider} from './contexts/LogContext';
+import {SearchContextProvider} from './contexts/SearchContext';
 
 function App() {
   return (
     <NavigationContainer>
-      <LogContextProvider>
-        <RootStack />
-      </LogContextProvider>
+      <SearchContextProvider>
+        <LogContextProvider>
+          <RootStack />
+        </LogContextProvider>
+      </SearchContextProvider>
     </NavigationContainer>
   );
 }
--- ReactNative/dealing-with-react-native/DayLog/screens/MainTab.js
+++ ReactNative/dealing-with-react-native/DayLog/screens/MainTab.js
@@ -4,6 +4,7 @@ import FeedsScreen from './FeedsScreen';
 import CalendarScreen from './CalendarScreen';
 import SearchScreen from './SearchScreen';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import SearchHeader from '../components/SearchHeader';
 
 const Tab = createBottomTabNavigator();
 
@@ -38,10 +39,13 @@ function MainTab() {
         name="Search"
         component={SearchScreen}
         options={{
+          title: '검색',
           // eslint-disable-next-line react/no-unstable-nested-components
           tabBarIcon: ({color, size}) => (
             <Icon name="search" size={size} color={color} />
           ),
+          // eslint-disable-next-line react/no-unstable-nested-components
+          headerTitle: () => <SearchHeader />,
         }}
       />
     </Tab.Navigator>
