8d3dd227aecfcc7d83c4eeb3c618b6559380e79b
bohongu <jaehong2782@naver.com>
2023-05-19 12:36:03 +0900

    feat: solve bkj_2446

---
+++ Algorithm/Baekjoon/2446.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "/input.txt";
+const input = fs
+  .readFileSync(__dirname + filePath)
+  .toString()
+  .trim();
+
+let result = "";
+for (let i = input; i > 0; i--) {
+  result = " ".repeat(input - i) + "*".repeat(2 * i - 1);
+  console.log(result);
+}
+
+for (let i = 2; i <= input; i++) {
+  result = " ".repeat(input - i) + "*".repeat(2 * i - 1);
+  console.log(result);
+}
