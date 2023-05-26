a344c434d27b1867375d300dbf6a490a8b91a231
bohongu <jaehong2782@naver.com>
2023-05-26 23:29:43 +0900

    feat: LearnTypeScript

---
+++ ReactNative/dealing-with-react-native/LearnTypeScript/learn.ts
@@ -0,0 +1,21 @@
+export default function () {}
+
+class Queue<T> {
+  list: T[] = [];
+  get length() {
+    return this.list.length;
+  }
+  enqueue(item: T) {
+    this.list.push(item);
+  }
+
+  dequeue() {
+    return this.list.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+const first = queue.dequeue();
+const second = queue.dequeue();
---
+++ ReactNative/dealing-with-react-native/LearnTypeScript/screens/RootStack.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import {
+  NativeStackNavigationProp,
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
+import {Button, Text, View} from 'react-native';
+import MainTab, {MainTabNavigationScreenParams} from './MainTab';
+
+type RootStackParamList = {
+  MainTab: MainTabNavigationScreenParams;
+  Detail: {id: number};
+};
+
+export type RootStackNavigationProp =
+  NativeStackNavigationProp<RootStackParamList>;
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+type DetailScreenRouteProp = RouteProp<RootStackParamList, 'Detail'>;
+
+function DetailScreen() {
+  const {params} = useRoute<DetailScreenRouteProp>();
+  return (
+    <View>
+      <Text>Detail {params.id}</Text>
+    </View>
+  );
+}
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        component={MainTab}
+        name="MainTab"
+        options={{headerShown: false}}
+      />
+      <Stack.Screen component={DetailScreen} name="Detail" />
+    </Stack.Navigator>
+  );
+}
+
+export default RootStack;
