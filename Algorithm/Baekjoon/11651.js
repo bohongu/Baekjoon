a36846d62130fc1134e45f067264ed85534362af
bohongu <jaehong2782@naver.com>
2023-05-31 09:56:20 +0900

    feat: solve bkj_11651

---
+++ Algorithm/Baekjoon/11651.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "/input.txt";
+const input = fs
+  .readFileSync(__dirname + filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = +input.shift();
+
+console.log(
+  input
+    .sort((a, b) =>
+      a.split(" ")[1] === b.split(" ")[1]
+        ? a.split(" ")[0] - b.split(" ")[0]
+        : a.split(" ")[1] - b.split(" ")[1]
+    )
+    .join("\n")
+);
